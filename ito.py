96dbbf4f9a8e796feb7d3c72913af5a4520be653
nfsri <nfsrivals757@gmail.com>
2020-12-03 19:28:16 +0300

    ito

---
+++ ito.py
@@ -0,0 +1,23 @@
+films = ['Во все тяжкое', 'Трансформеры', 'Приколи', 'Американский пирог', 'Непосредственно Каха', 'Люди в черном', 'Довод']
+
+print(films[5])
+
+print(films[len(films) - 3])
+print(films[-3])
+
+print(films[-1])
+print(films[len(films) - 1])
+
+films.append('Смурфики')
+print(films)
+
+films.insert(2, 'Игра эндера')
+print(films)
+
+last_film = films.pop()
+print(last_film)
+print(films)
+
+print(films.count('Довод'))
+
+print(films.index('Непосредственно Каха'))
